7c516ec078a984393d5f92ef966bfe20b1da2677
noah <noahwalsmits@telfort.nl>
2023-03-22 21:03:13 +0100

    added search form for achievements by game

--- src/app/achievements-by-game/achievements-by-game.component.ts
+++ src/app/achievements-by-game/achievements-by-game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { BackendService } from '../backend.service';
 
 @Component({
   selector: 'app-achievements-by-game',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achievements-by-game.component.css']
 })
 export class AchievementsByGameComponent implements OnInit {
+  searchForm = this.formBuilder.group({appId: '' });
+  errorMessage?: string;
 
-  constructor() { }
+  constructor(private backendService: BackendService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.errorMessage = undefined;
+    let appId: number = Number(this.searchForm.value.appId)
+    if(isNaN(appId) || this.searchForm.value.appId.length === 0) {
+      this.errorMessage = 'app id must be a number';
+      return;
+    }
+    //TODO make request to backend
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { AchievementItemComponent } from './achievement-item/achievement-item.co
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
